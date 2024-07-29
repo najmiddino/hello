3ea2644b43b2fe0d38f7e94b0ba998e8c81976a0
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-07-29 22:06:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function displayCode() {
+    const userInput = document.getElementById('userInput').value;
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = userInput;
+    }
